713e2b24d799f6c19276e193d7d3f71f69c89c16
Moshe Zada <zada.moshe@gmail.com>
2019-09-01 13:06:13 +0300

    Add title

--- src/App.vue
+++ src/App.vue
@@ -34,7 +34,7 @@ export default {
   },
   data() {
     return {
-      darkMode: true
+      darkMode: false
     };
   }
 };
--- src/components/Base/MToolbar.vue
+++ src/components/Base/MToolbar.vue
@@ -1,12 +1,9 @@
 <template>
   <v-toolbar app>
-    <v-switch
-      v-model="darkMode"
-      color="grey darken-2"
-      hide-details
-      prepend-icon="wb_sunny"
-      append-icon="brightness_3"
-    />
+    <div class="mr-4 chmod">
+      chmod +x ./moshe.sh
+    </div>
+
     <v-spacer />
     <v-toolbar-items>
       <v-btn
@@ -21,6 +18,15 @@
       </v-btn>
     </v-toolbar-items>
     <v-spacer />
+    <div>
+      <v-switch
+        v-model="darkMode"
+        color="grey darken-2"
+        hide-details
+        prepend-icon="wb_sunny"
+        append-icon="brightness_3"
+      />
+    </div>
   </v-toolbar>
 </template>
 
@@ -34,7 +40,7 @@ export default {
         { name: "open source", id: "#open-source" },
         { name: "technical skills", id: "#technical-skills" }
       ],
-      darkMode: true
+      darkMode: false
     };
   },
   watch: {
@@ -45,4 +51,48 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.chmod {
+  /* font-family: monospace; */
+  font-family: Monaco, Arial, Helvetica, sans-serif;
+  font-size: 20px;
+  line-height: 20px;
+  position: relative;
+}
+
+.chmod::after {
+  content: "";
+  position: absolute;
+  top: 0;
+  right: -20px;
+  background-color: #00c853;
+  width: 10px;
+  height: 20px;
+  -webkit-animation: blink 1s step-end infinite;
+  animation: blink 1s step-end infinite;
+}
+
+@-webkit-keyframes blink {
+  0% {
+    opacity: 1;
+  }
+  50% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
+}
+
+@keyframes blink {
+  0% {
+    opacity: 1;
+  }
+  50% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
+}
+</style>
