44931018b2e97238a859dd15e0b6f34eb088ff10
Moshe Zada <zada.moshe@gmail.com>
2019-09-01 22:35:33 +0300

    Grammer fixes

--- src/components/Sections/AboutMeSection.vue
+++ src/components/Sections/AboutMeSection.vue
@@ -2,9 +2,9 @@
   <m-section title="About Me">
     My name is Moshe, I love to code, from deployment scripts to database admin
     dashboards and mobile applications.
-    <br />In the last 4 and a half years, I worked at
-    <a href="https://forter.com">forter</a> as a team member, team leader (Data
-    science and backend) and internal tools developer.<br />I served in at the
+    <br />In the last four and a half years, I worked at
+    <a href="https://forter.com">Forter</a> as a team member, team leader (data
+    science and backend), and internal tools developer.<br />I served in an
     enterprise SRE team of an elite branch in the 8200 alumni.
     <br />
     <br />
