e8470f7296268c4723c9fb24d5469a77bfc43eca
Moshe Zada <zada.moshe@gmail.com>
2019-09-01 19:24:55 +0300

    lottie animations!

--- src/components/SideSections/SocialNetworksSection.vue
+++ src/components/SideSections/SocialNetworksSection.vue
@@ -2,67 +2,82 @@
   <div>
     <section-header icon="people">Social Networks</section-header>
     <v-layout row class="text-center mt-2">
-      <v-flex v-for="social in socials" :key="social.name">
-        <v-btn class="elevation-6" icon :href="social.href">
-          <svg
-            role="img"
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
+      <v-flex v-for="social in socials" :key="social.id">
+        <v-btn class="elevation-6" icon :href="social.href" color="primary">
+          <div
+            :id="social.id"
+            @mouseover="social.obj.play()"
+            @mouseleave="social.obj.stop()"
             style="height: 20px;"
-          >
-            <title>{{ social.name }}</title>
-            <path :d="social.d"></path>
-          </svg>
+          />
         </v-btn>
       </v-flex>
+      <v-btn class="elevation-6" icon :href="so.href" color="primary">
+        <svg
+          role="img"
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+          style="height: 20px;fill:white"
+        >
+          <title>{{ so.name }}</title>
+          <path :d="so.d"></path>
+        </svg>
+      </v-btn>
     </v-layout>
   </div>
 </template>
 
 <script>
 import SectionHeader from "../Base/SectionHeader.vue";
+import bodymovin from "lottie-web";
 
 export default {
-  name: "SocialNetworksSection",
   components: { SectionHeader },
+  mounted() {
+    this.socials.map(
+      x =>
+        (x.obj = bodymovin.loadAnimation({
+          container: document.getElementById(x.id), // Required
+          path: `${x.id}.json`,
+          renderer: "svg",
+          loop: true,
+          autoplay: false
+        }))
+    );
+    const twitter = this.socials[0].obj;
+    twitter.play();
+    setTimeout(() => {
+      twitter.stop();
+    }, 4000);
+  },
   data() {
     return {
       socials: [
         {
-          name: "Twitter",
-          href: "https://twitter.com/moshe_zada",
-          d:
-            "M23.954 4.569c-.885.389-1.83.654-2.825.775 1.014-.611 1.794-1.574 2.163-2.723-.951.555-2.005.959-3.127 1.184-.896-.959-2.173-1.559-3.591-1.559-2.717 0-4.92 2.203-4.92 4.917 0 .39.045.765.127 1.124C7.691 8.094 4.066 6.13 1.64 3.161c-.427.722-.666 1.561-.666 2.475 0 1.71.87 3.213 2.188 4.096-.807-.026-1.566-.248-2.228-.616v.061c0 2.385 1.693 4.374 3.946 4.827-.413.111-.849.171-1.296.171-.314 0-.615-.03-.916-.086.631 1.953 2.445 3.377 4.604 3.417-1.68 1.319-3.809 2.105-6.102 2.105-.39 0-.779-.023-1.17-.067 2.189 1.394 4.768 2.209 7.557 2.209 9.054 0 13.999-7.496 13.999-13.986 0-.209 0-.42-.015-.63.961-.689 1.8-1.56 2.46-2.548l-.047-.02z"
+          obj: null,
+          id: "twitter",
+          href: "https://twitter.com/moshe_zada"
         },
         {
-          name: "GitHub",
+          id: "github",
           href: "https://github.com/moshe",
-          d:
-            "M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12"
+          obj: null
         },
         {
-          name: "LinkedIn",
+          id: "linkedin",
           href: "https://www.linkedin.com/in/moshe-zada",
-          d:
-            "M4.98 3.5c0 1.381-1.11 2.5-2.48 2.5s-2.48-1.119-2.48-2.5c0-1.38 1.11-2.5 2.48-2.5s2.48 1.12 2.48 2.5zm.02 4.5h-5v16h5v-16zm7.982 0h-4.968v16h4.969v-8.399c0-4.67 6.029-5.052 6.029 0v8.399h4.988v-10.131c0-7.88-8.922-7.593-11.018-3.714v-2.155z"
-        },
-        {
-          name: "Stack Overflow",
-          href: "https://stackoverflow.com/users/4540123/moshezada",
-          d:
-            "M15 21h-10v-2h10v2zm6-11.665l-1.621-9.335-1.993.346 1.62 9.335 1.994-.346zm-5.964 6.937l-9.746-.975-.186 2.016 9.755.879.177-1.92zm.538-2.587l-9.276-2.608-.526 1.954 9.306 2.5.496-1.846zm1.204-2.413l-8.297-4.864-1.029 1.743 8.298 4.865 1.028-1.744zm1.866-1.467l-5.339-7.829-1.672 1.14 5.339 7.829 1.672-1.14zm-2.644 4.195v8h-12v-8h-2v10h16v-10h-2z"
+          obj: null
         }
-      ]
+      ],
+      so: {
+        name: "Stack Overflow",
+        href: "https://stackoverflow.com/users/4540123/moshezada",
+        d:
+          "M15 21h-10v-2h10v2zm6-11.665l-1.621-9.335-1.993.346 1.62 9.335 1.994-.346zm-5.964 6.937l-9.746-.975-.186 2.016 9.755.879.177-1.92zm.538-2.587l-9.276-2.608-.526 1.954 9.306 2.5.496-1.846zm1.204-2.413l-8.297-4.864-1.029 1.743 8.298 4.865 1.028-1.744zm1.866-1.467l-5.339-7.829-1.672 1.14 5.339 7.829 1.672-1.14zm-2.644 4.195v8h-12v-8h-2v10h16v-10h-2z"
+      }
     };
   }
 };
 </script>
 
-<style scoped>
-.theme--light path {
-  fill: #0288d1;
-}
-.theme--dark path {
-  fill: #4fc3f7;
-}
-</style>
+<style scoped></style>
