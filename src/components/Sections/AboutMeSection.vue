ce20e17c0cd6ec442874a5208bf016deb49aa202
Moshe Zada <zada.moshe@gmail.com>
2019-09-01 15:11:38 +0300

    Add about me content

--- src/components/Sections/AboutMeSection.vue
+++ src/components/Sections/AboutMeSection.vue
@@ -1,16 +1,16 @@
 <template>
   <m-section title="About Me">
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin nibh augue,
-    suscipit a, scelerisque sed, lacinia in, mi. Cras vel lorem. Etiam
-    pellentesque aliquet tellus. Phasellus pharetra nulla ac diam.
+    My name is Moshe, I love to code everything, from deployment scripts to
+    database admin dashboards and mobile applications.
+    <br />In the last 4 and a half years, I worked at
+    <a href="https://forter.com">forter</a> as a team member, team leader (Data
+    science and backend) and internal tools developer.<br />I served in at the
+    enterprise SRE team of an elite branch in the 8200 alumni.
     <br />
     <br />
-    Quisque semper justo at risus. Donec venenatis, turpis vel hendrerit
-    interdum, dui ligula ultricies purus, sed posuere libero dui id orci. Nam
-    congue, pede vitae dapibus aliquet, elit magna vulputate arcu, vel tempus
-    metus leo non est. Etiam sit amet lectus quis est congue mollis. Phasellus
-    congue lacus eget neque. Phasellus ornare, ante vitae consectetuer
-    consequat, purus sapien ultricies dolor, et mollis pede metus eget nisi.
+    Whether you need a fast prototype of a product, speed up your CI environment
+    or build an internal tool to boost your employee work, drop me a line and
+    let's chat 🙂
   </m-section>
 </template>
 
