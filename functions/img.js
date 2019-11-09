58a10589d12620233718c8f7c35d9fe6fab4ad17
Moshe Zada <zada.moshe@gmail.com>
2019-11-09 23:09:08 +0200

    Send to google analytics

--- functions/img.js
+++ functions/img.js
@@ -1,3 +1,5 @@
+var ua = require("universal-analytics");
+
 const pixel =
   "iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAABGdBTUEAALGPC/xhBQAAACBjSFJNAAB6JgAAgIQAAPoAAACA6AAAdTAAAOpgAAA6m" +
   "AAAF3CculE8AAACC2lUWHRYTUw6Y29tLmFkb2JlLnhtcAAAAAAAPHg6eG1wbWV0YSB4bWxuczp4PSJhZG9iZTpuczptZXRhLyIgeDp4bXB0az0iWE" +
@@ -8,8 +10,73 @@ const pixel =
   "ZmOlBob3RvbWV0cmljSW50ZXJwcmV0YXRpb24+MjwvdGlmZjpQaG90b21ldHJpY0ludGVycHJldGF0aW9uPgogICAgICA8L3JkZjpEZXNjcmlwdGl" +
   "vbj4KICAgPC9yZGY6UkRGPgo8L3g6eG1wbWV0YT4KD0UqkwAAAAtJREFUCB1j+A8EAAn7A/0Mu1vnAAAAAElFTkSuQmCC";
 
+function toQueryString(json) {
+  return (
+    "?" +
+    Object.keys(json)
+      .map(function(key) {
+        return encodeURIComponent(key) + "=" + encodeURIComponent(json[key]);
+      })
+      .join("&")
+  );
+}
+function githubEvent(event) {
+  return {
+    userLanguage: event.headers["x-language"]
+      ? event.headers["x-language"].split(",")[0]
+      : undefined,
+    documentReferrer: "https://github.com/moshe/elasticsearch-comrade",
+    documentPath: `/${toQueryString(event.queryStringParameters)}`,
+    documentTitle: "Github",
+    documentHostName: "https://github.com",
+    anonymizeIp: true,
+    userAgentOverride: ""
+  };
+}
+
+function genericEvent(event) {
+  let refParams = {
+    documentTitle: "None",
+    documentPath: `/${toQueryString(event.queryStringParameters)}`
+  };
+  if (event.headers.referer) {
+    const url = new URL(event.headers.referer);
+    refParams = {
+      documentTitle: url.hostname,
+      documentHostName: url.hostname,
+      documentPath: `${url.pathname}/${toQueryString(
+        event.queryStringParameters
+      )}`
+    };
+  }
+  return Object.assign(
+    {
+      userLanguage: event.headers["x-language"]
+        ? event.headers["x-language"].split(",")[0]
+        : undefined,
+      ipOverride: event.headers["client-ip"],
+      userId: event.headers.cookie,
+      documentReferrer: event.headers.referer,
+      userAgentOverride: event.headers["user-agent"],
+      geoid: event.headers["x-country"]
+    },
+    refParams
+  );
+}
+
 export function handler(event, context, callback) {
-  console.log(JSON.stringify(event));
+  const visitor = ua("UA-146865947-2");
+  let GAEvent;
+  const isGitHub = event.headers["user-agent"].startsWith("github-camo");
+  if (isGitHub) {
+    GAEvent = githubEvent(event);
+    visitor.pageview(GAEvent, console.log);
+    console.log(`Sending github event ${JSON.stringify(GAEvent)}`);
+  } else {
+    GAEvent = genericEvent(event);
+    visitor.pageview(GAEvent, console.log);
+    console.log(`Sending generic event ${JSON.stringify(GAEvent)}`);
+  }
   callback(null, {
     statusCode: 200,
     body: pixel,
@@ -19,37 +86,3 @@ export function handler(event, context, callback) {
     isBase64Encoded: true
   });
 }
-
-const a = {
-  path: "/.netlify/functions/img",
-  httpMethod: "GET",
-  headers: {
-    accept:
-      "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3",
-    "accept-encoding": "br, gzip",
-    "accept-language": "en-US,en;q=0.9,he;q=0.8",
-    "cache-control": "no-cache",
-    "client-ip": "141.226.145.176",
-    cookie: "_ga=GA1.2.131657",
-    pragma: "no-cache",
-    "sec-fetch-mode": "navigate",
-    "sec-fetch-site": "none",
-    "sec-fetch-user": "?1",
-    "upgrade-insecure-requests": "1",
-    "user-agent":
-      "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.70 Safari/537.36",
-    via: "https/2 Netlify[fd728efc-2377-4cb64] (ApacheTrafficServer/7.1.7)",
-    "x-bb-ab": "0.160439",
-    "x-bb-client-request-uuid": "fd728efc-2377-4c2d-9b29",
-    "x-bb-ip": "141.226.145.176",
-    "x-bb-loop": "1",
-    "x-country": "IL",
-    "x-forwarded-for": "141.226.145.176",
-    "x-forwarded-proto": "https",
-    "x-language": "en,en;q=0.9,he;q=0.8",
-    "x-nf-client-connection-ip": "141.226.145.176"
-  },
-  queryStringParameters: {},
-  body: "",
-  isBase64Encoded: true
-};
